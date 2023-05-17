08a037e3f5f7e5cc541ecadebb3ddebb905b7ac4
DeGenius360 <degenius8@gmail.com>
2023-05-17 23:27:52 +0100

    Solved Palindrome Number

---
+++ Problems&Solutions/Easy/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        String numberToString = String.valueOf(x);
+        int numberLength = numberToString.length();
+        int left = 0;
+        int right = numberLength - 1;
+        while (left < right){
+            if ( numberToString.charAt(left) != numberToString.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int x = -121;
+        int y = 121;
+        System.out.println(isPalindrome(x));
+        System.out.println(isPalindrome(y));
+    }
+}
